94cad36ee05b9baaff45ee92e2158f4d65e08943
hortifrutibrasileiros <91627384+hortifrutibrasileiros@users.noreply.github.com>
2024-01-27 17:52:02 -0300

    Add files via upload

---
+++ fiscal/service-worker.js
@@ -0,0 +1,24 @@
+// service-worker.js
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('brasileiros-hortifruti-fiscal-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/icon.png',
+        '/styles.css',
+        '/list.js',
+        '/scripts.js',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
