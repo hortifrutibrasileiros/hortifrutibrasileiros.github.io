5d3f653aa86c7c569e9130f9245bff3ee793ddec
hortifrutibrasileiros <91627384+hortifrutibrasileiros@users.noreply.github.com>
2024-01-28 02:44:22 -0300

    Update service-worker.js

--- fiscal/service-worker.js
+++ fiscal/service-worker.js
@@ -1,21 +1,26 @@
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-      caches.open('bh-adm-listedit-cache').then((cache) => {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/manifest.json',
-          '/icon.png',
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', (event) => {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
+  event.waitUntil(
+    caches.open('brasileiros-hortifruti-fiscal-cache').then((cache) => {
+      return cache.addAll([
+        'index.html',
+        'manifest.json',
+        'icon.png',
+        'styles.css',
+        'list.js',
+        'scripts.js',
+        'editlist/index.html',
+        'editlist.png',
+        'favicon.ico',
+        'icons-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
