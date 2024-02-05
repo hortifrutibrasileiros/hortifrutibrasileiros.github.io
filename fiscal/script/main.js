1b6e9a1c7588be329e3f2f6951530684be0446e4
hortifrutibrasileiros <91627384+hortifrutibrasileiros@users.noreply.github.com>
2024-02-05 09:54:10 -0300

    Update main.js

--- fiscal/script/main.js
+++ fiscal/script/main.js
@@ -430,8 +430,8 @@ function generateQRCode() {
     var rawPixValue = $('.total').text();
     var pixValue = parseFloat(rawPixValue.replace('R$', '').trim().replace(',', '.')) || 0;
 
-    var pixKey = '03286015431'; // Altere para qualquer chave PIX: Celular, CPF, CNPJ ou chave aleatória.
-    var destinatario = 'EDNA LUCIA SOUTO BRASILEIRO'; // Digite aqui o destinatário
+    var pixKey = '+5583982145645'; // Altere para qualquer chave PIX: Celular, CPF, CNPJ ou chave aleatória.
+    var destinatario = 'THIAGO SOUTO BRASILEIRO'; // Digite aqui o destinatário
     var cidade = 'SAO PAULO'; // Digite aqui a cidade com máximo de 24 caracteres
 
     // Construindo o Payload PIX a partir dos dados adicionados.
